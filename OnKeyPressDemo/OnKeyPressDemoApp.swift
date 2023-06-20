721f654b101fec26f49bb83e7e40f56dc66152a2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-20 11:23:39 -0500

    Initial Commit

---
+++ OnKeyPressDemo/OnKeyPressDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnKeyPressDemoApp.swift
+//  OnKeyPressDemo
+//
+//  Created by Mark Volkmann on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct OnKeyPressDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
